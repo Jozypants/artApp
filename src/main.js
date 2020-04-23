64c48fa4d6b74f634d7a3dc2c31a1c308b2f6969
Jozy Kinnaman <jozypants@gmail.com>
2020-04-23 13:12:00 -0700

    add js back end and front end logic with api call.

--- src/main.js
+++ src/main.js
@@ -2,3 +2,15 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 import $ from "jquery";
+import {ArtService} from "./artApp.js";
+
+
+$(document).ready(function (){
+  
+  (async () => {
+    let artService = new ArtService();
+    // eslint-disable-next-line 
+    const response = await artService.getArt();
+    let {title, people, images} = response;
+  })();
+});
